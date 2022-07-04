27ee769552560dc85abf2858b8fa3daedf60ca7f
Mahdi Hasan Khan <mhk1121992@gmail.com>
2022-07-04 12:22:46 +0600

    Create print_math.py

---
+++ print_math.py
@@ -0,0 +1,16 @@
+# Write a function to print the following:
+# 1 2 3 4 5 6 7 8 9 10 9 8 7 6 5 4 3 2 1
+
+def print_math():
+    num = 10
+    n = 0
+    for n in range(num ):
+        print(n + 1)
+    
+    for i in range(n):
+        print(n)
+        n-=1
+    
+   
+
+print_math()
