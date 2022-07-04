72faf00ea8bdb80bf8af4fd8861d2ebb51e4efeb
Mahdi Hasan Khan <mhk1121992@gmail.com>
2022-07-04 12:32:24 +0600

    Create string_reverse.py

---
+++ string_reverse.py
@@ -0,0 +1,16 @@
+# Solution 1
+
+def string_reverse(string):
+    return string[::-1]
+    
+
+print(string_reverse("mahdi"))
+
+
+# Solution 2
+
+def reverse(x):
+    return "".join(reversed(x))
+    
+
+print (reverse("HASAN"))
